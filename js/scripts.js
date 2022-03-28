2dc32e704ce6ffff59ceed9a24e5e0fcea1a7990
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-03-28 11:44:20 -0700

    add a working script to display list in order and uppercase.
    Co-authored-by: Jeremy Martin <jeremyvs1945@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,21 @@
 $(document).ready(function() {
-  $("#formOne").submit(function(event) {
+  $("#shopping-list").submit(function(event) {
     event.preventDefault();
     const list1 = $("#item1").val();
     const list2 = $("#item2").val();
     const list3 = $("#item3").val();
 
     let fullList = [list1, list2, list3];
+    fullList.sort();
+    let upperList = [];
+    
+    fullList.forEach(function(element) {
+      upperList.push(element.toUpperCase());
+    });
 
-    fullList.forEach(function(element))
-
-
-
+    upperList.forEach(function(element) {
+      $("#list-display").append("<li>" + element + "</li>");
+      $("#shopping-list").hide();
+    });
   });
 });
